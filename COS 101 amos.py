4fa72e67314eaa63b6e3ba366e7258323074a01c
amos5464 <amosoyeyemi84@gmail.com>
2024-11-18 02:17:15 +0100

    Add files via upload

---
+++ COS 101 amos.py
@@ -0,0 +1,30 @@
+#Kinetic Energy = 1/2 × mass × velocity^2)
+mass = float(input('enter mass: '))
+velocity = float(input('enter velocity: '))
+kineticenergy = 1/2*mass*velocity**2
+print('the kineticenergy of the object is = ', kineticenergy)
+
+#Force = mass * acceleration
+mass = float(input('enter mass: '))
+acceleration = float(input('enter acceleration: '))
+Force = mass*acceleration
+print('the force of the object is = ', Force)
+
+#Potential Energy = mass * gravity * height
+mass = float(input('enter mass: '))
+gravity = float(input('enter gravity: '))
+height = float(input('enter height'))
+PotentialEnergy = mass * gravity * height
+print('the Potential Energy is = ', PotentialEnergy)
+
+#Momentum = mass * velocity
+mass = float(input('enter mass: '))
+Velocity = float(input('enter velocity: ')) 
+Momentum = mass * velocity
+print('the Momentum is = ', Momentum)
+
+#Velocity = distance / time
+distance = float(input('enter distance: '))
+time = float(input('enter time: '))
+Velocity = distance / time
+print('the Velocity is = ', Velocity )
